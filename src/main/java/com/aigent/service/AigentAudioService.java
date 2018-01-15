b80b26a90e15810573d0a9cb0afab28186f31d35
Oleksii Babka <oleksii.babka@epay.ingenico.com>
2018-01-15 14:56:25 +0100

    small fix of array casting

--- src/main/java/com/aigent/service/AigentAudioService.java
+++ src/main/java/com/aigent/service/AigentAudioService.java
@@ -1,6 +1,8 @@
 package com.aigent.service;
 
 import com.aigent.service.speech.SpeechProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,8 @@ import java.util.List;
 
 @SpringBootApplication
 public class AigentAudioService implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(AigentAudioService.class);
+
     @Autowired
     private SpeechProcessor speechProcessor;
     @Autowired
@@ -24,7 +28,10 @@ public class AigentAudioService implements CommandLineRunner {
         String fileName = "130106-09-05-16-removed-private-information.wav";
         List<String> recognizedSpeech = speechProcessor.recognizeSpeech(fileName);
 
-        restClient.callNounsUrl(fileName, (String[]) recognizedSpeech.toArray());
+        RestClient.Response response = restClient.callNounsUrl(fileName, recognizedSpeech.toArray(new String[recognizedSpeech.size()]));
+
+        LOG.info("Http response code: " + response.responseCode);
+        LOG.info("Http response: " + response.response);
     }
 
 }
--- src/main/java/com/aigent/service/config/SphinxConfig.java
+++ src/main/java/com/aigent/service/config/SphinxConfig.java
@@ -12,6 +12,8 @@ public class SphinxConfig {
     public StreamSpeechRecognizer speechRecognizer() throws IOException {
         edu.cmu.sphinx.api.Configuration configuration = new edu.cmu.sphinx.api.Configuration();
 
+        configuration.setSampleRate(8000);
+
         configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
         configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
         configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
