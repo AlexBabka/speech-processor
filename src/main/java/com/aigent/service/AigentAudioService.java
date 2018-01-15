0169276b4a3fd8876747bac2a99998da8e322000
Oleksii Babka <oleksii.babka@epay.ingenico.com>
2018-01-15 12:21:40 +0100

    first push of project

---
+++ src/main/java/com/aigent/service/AigentAudioService.java
@@ -0,0 +1,14 @@
+package com.aigent.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class AigentAudioService {
+
+	public static void main(String[] args) {
+			 SpringApplication app = new SpringApplication(AigentAudioService.class);
+		}
+}
